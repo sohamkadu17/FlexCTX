af25b78f7e152c70084e6a0e2320850df57af326
sohamkadu17 <kadusoham91@gmail.com>
2026-09-01 18:34:47 +0530

    feat(api): export demo router in API package

--- router/api/__init__.py
+++ router/api/__init__.py
@@ -2,7 +2,8 @@
 
 from router.api.admin import router as admin_router
 from router.api.chat import router as chat_router
+from router.api.demo import router as demo_router
 from router.api.health import router as health_router
 from router.api.models import router as models_router
 
-__all__ = ["admin_router", "chat_router", "health_router", "models_router"]
+__all__ = ["admin_router", "chat_router", "demo_router", "health_router", "models_router"]
